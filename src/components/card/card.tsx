f61d4ea8790e9ed0f4897f6688b48bb1423d27fb
Phodal HUANG <h@phodal.com>
2019-08-19 15:49:48 +0800

    test: test useEffect API

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,11 +1,24 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 function PhdCard() {
   const [count, setCount] = useState(0);
+  useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `You clicked ${count} times`;
+    setIsOnline(count % 2 === 0);
+    return function cleanup() {
+      console.log('cleanup');
+    }
+  });
+
+  const [isOnline, setIsOnline] = useState(false);
+  useEffect(() => {
+    console.log('isOnline', isOnline);
+  });
 
   return (
     <div className="card">
-      Card {count}
+      Card {count}, {isOnline ? '1' : '0'} -
       <button onClick={() => setCount(count + 1)}>
         Click me
       </button>
